e439b37e866d51d32ca0a0234a3033b336c8c4b1
miyamae <yu1ro.mya@gmail.com>
2021-04-16 16:07:58 +0900

    add: next

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,13 +1,40 @@
-import './ExploreContainer.css';
+import React from 'react'
 
 interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
-    <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
+      <>
+          <div className="container">
+              <strong>Ready to create an app?</strong>
+              <p>Start with Ionic <a target="_blank" rel="noopener noreferrer"
+                                     href="https://ionicframework.com/docs/components">UI Components</a></p>
+          </div>
+          <style jsx>{`.container {
+            text-align: center;
+            position: absolute;
+            left: 0;
+            right: 0;
+            top: 50%;
+            transform: translateY(-50%);
+          }
+
+          .container strong {
+            font-size: 20px;
+            line-height: 26px;
+          }
+
+          .container p {
+            font-size: 16px;
+            line-height: 22px;
+            color: #8c8c8c;
+            margin: 0;
+          }
+
+          .container a {
+            text-decoration: none;
+          }`}</style>
+      </>
   );
 };
 
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,6 +1,5 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import ExploreContainer from '../ExploreContainer';
 
 const Home: React.FC = () => {
   return (
